0a78590f05be0ecd6fe73b264132e247ec70a44c
Shreyas <shreyaskumar.work@gmail.com>
2024-08-24 18:54:03 +0530

    Angular18 Tutorial - Till Step 7

---
+++ src/app/routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { DetailsComponent } from "./details/details.component";
+
+const routeConfig: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    title: "Home page",
+  },
+  {
+    path: "details/:id",
+    component: DetailsComponent,
+    title: "Home details",
+  },
+  // {
+  //   path: "details",
+  //   component: DetailsComponent,
+  //   title: "Details Page"
+  // }
+];
+export default routeConfig;
